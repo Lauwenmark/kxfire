b5edf52142513125505dc4d4368298efec24dd27
lauwenmark <lauwenmark@gmail.com>
2018-06-24 22:00:15 +0200

    Properly store components in entities into a HashMap, using their class as key.

--- server/src/main/kotlin/Entities.kt
+++ server/src/main/kotlin/Entities.kt
@@ -3,5 +3,5 @@ package eu.lauwenmark.jxfire.server.entities
 import eu.lauwenmark.jxfire.server.components.Component
 
 class GameEntity {
-    val components = HashSet<Component>()
+    val components = HashMap<Class<Component>, Component>()
 }
