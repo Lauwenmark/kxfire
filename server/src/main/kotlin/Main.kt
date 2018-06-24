19f8946b514d9ef25f9f7a2f4ddc1296d6e3e691
lauwenmark <lauwenmark@gmail.com>
2018-06-24 22:04:45 +0200

    Moved playfield related classes in their own package and file.

--- server/src/main/kotlin/Main.kt
+++ server/src/main/kotlin/Main.kt
@@ -1,29 +1,11 @@
 package eu.lauwenmark.jxfire.server
 
-import eu.lauwenmark.jxfire.server.entities.GameEntity
 import eu.lauwenmark.jxfire.server.events.createEventQueue
 import eu.lauwenmark.jxfire.server.services.TickService
 import eu.lauwenmark.jxfire.server.services.registerService
-import java.math.BigInteger
 
 fun main(args: Array<String>) {
     System.out.println("Hello")
     createEventQueue("main")
     registerService(TickService())
 }
-
-interface Playfield {
-    fun square(x: BigInteger, y: BigInteger) : List<GameEntity>
-}
-
-class TrivialPlayfield(val width: Int, val height: Int) : Playfield {
-    val content : Array<Array<List<GameEntity>>> = Array<Array<List<GameEntity>>>(width) {
-        Array<List<GameEntity>>(height) {
-            ArrayList<GameEntity>()
-        }
-    }
-    override fun square(x: BigInteger, y: BigInteger): List<GameEntity> {
-        return content[x.intValueExact()][y.intValueExact()]
-    }
-}
-
